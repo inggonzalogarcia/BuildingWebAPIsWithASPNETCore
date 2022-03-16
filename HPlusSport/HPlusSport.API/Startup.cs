ab8006655c275d62f78fb7ee06ce1ae8b0b46585
gonzalo.garcia <gonzalo.garcia@ascentis.com>
2022-03-15 18:43:59 -0700

    6.Securing APIs

--- HPlusSport/HPlusSport.API/Startup.cs
+++ HPlusSport/HPlusSport.API/Startup.cs
@@ -35,6 +35,16 @@ namespace HPlusSport.API
                 }
                 );
 
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins("https://localhost:44315")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
+
             services.AddApiVersioning(options => {
                 options.ReportApiVersions = true;
                 options.DefaultApiVersion = new ApiVersion(1, 0);
@@ -60,6 +70,8 @@ namespace HPlusSport.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
---
+++ HPlusSport/HPlusSport.Web/wwwroot/js/api.js
@@ -0,0 +1,22 @@
+﻿var url = "https://localhost:44315/products";
+
+var productsList = document.getElementById("products-list");
+if (productsList) {
+    fetch(url)
+        .then(response => reponse.json())
+        .then(data => showProducts(data))
+        .catch(ex => {
+            alert("Something went wrong ...");
+            console.log(ex);
+        });
+}
+
+function showProducts(products) {
+    products.forEach(product => {
+        let li = document.createElement("li");
+        let text = `${producname} ($${product.price})`;
+        li.appendChild(documencreateTextNode(text));
+        productsList.appendChild(li);
+        productsList.appendChild(li);
+    })
+}
